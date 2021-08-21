853afb10c0753575fe37902cd20f16462801fe39
Marcos <marcosbastidas301@gmail.com>
2021-08-21 07:54:42 -0400

    architecture_and_component_views

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <p className="footer-title">
+                PlatziConf merch
+            </p>
+            <p className="footer-copy">
+                Todos los izquierdos reservados
+            </p>
+        </div>
+    )
+}
+
+export default Footer
